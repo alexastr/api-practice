75b89e74ffc5404d87b3607a0161e1829c3f9e22
Alexa Strobel <alexa.strobel@gmail.com>
2017-12-29 14:31:19 +0800

    initial commit test

---
+++ apps.js
@@ -0,0 +1,14 @@
+$(function() {
+
+//function to access youtube API
+	$(document).on('click','#search-button',function(event){
+		
+	})
+
+//function to retrieve and display search results
+
+
+//function to make search results clickable
+
+
+})
